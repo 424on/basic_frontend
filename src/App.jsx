484755b504b1b474c231fa18b3483699cc37c19b
424on <88828652+424on@users.noreply.github.com>
2025-11-13 22:16:50 +0900

    메인 페이지 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-blue-200 text-3xl font-bold">
-            tailwind
+            테일 윈드
         </div>
     );
 };
